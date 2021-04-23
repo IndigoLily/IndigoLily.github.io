4e1bc6ed010a22413e903362fe02524b7a2052cf
lily <yulex.42@gmail.com>
2021-04-23 13:09:06 -0300

    recoloured juggle2 to match juggle, changed timing and other styling things

--- projects/juggle2/sketch.js
+++ projects/juggle2/sketch.js
@@ -1,14 +1,6 @@
 const cnv = document.body.appendChild(document.createElement('canvas'));
 const ctx = cnv.getContext('2d');
 
-function ease(t, p) {
-    if (t < 0.5) {
-	return (2*t)**p / 2;
-    } else {
-	return 1 - (2-2*t)**p/2;
-    }
-}
-
 const r = (3 - Math.sqrt(3))/4;
 
 function swap(arr, idx1, idx2) {
@@ -17,16 +9,16 @@ function swap(arr, idx1, idx2) {
     arr[idx2] = temp;
 }
 
-let w,h,
+let w,h,scale,
     t = 0
-    clrs = ['#f00', '#0f0', '#00f'],
+    clrs = ['hsl(0,0%,100%)', 'hsl(0,0%,50%)', 'hsl(0,0%,0%)'],
     pIdx = 0;
 function resize() {
     w = cnv.width  = window.innerWidth;
     h = cnv.height = window.innerHeight;
-    const scale = Math.min(w,h)/4;
+    scale = Math.min(w,h)/5;
     ctx.translate(w/2, h/2);
-    ctx.scale(scale, scale);
+    ctx.rotate(-Math.PI/2);
 }
 resize();
 window.addEventListener('resize', resize);
@@ -49,42 +41,48 @@ function stillPoint() {
 }
 
 function draw() {
-    const _t = ease(t, 3);
+    const s = 5;
+    const _t = (1 - Math.exp(-s * t)) / (1 - Math.exp(-s));
 
-    {
-	ctx.fillStyle = clrs[(pIdx+2)%3];
-	const p = stillPoint();
-	ctx.beginPath();
-	ctx.arc(p.x, p.y, r, 0, Math.PI*2);
-	ctx.fill();
-    }
+    for (let n = 0; n < 2; n++) {
+	ctx.shadowBlur = n==0 ? 20 : 0;
+	ctx.shadowColor = '#0004';
 
-    {
-	const pivot = getPivot();
-	const theta = pivotTheta() - Math.PI/2 + Math.PI*_t;
-	const p = {
-	    x: pivot.x + Math.cos(theta) * Math.sqrt(3)/2,
-	    y: pivot.y + Math.sin(theta) * Math.sqrt(3)/2,
-	};
+	{
+	    ctx.fillStyle = clrs[(pIdx+2)%3];
+	    const p = stillPoint();
+	    ctx.beginPath();
+	    ctx.arc(p.x * scale, p.y * scale, r * scale, 0, Math.PI*2);
+	    ctx.fill();
+	}
 
-	ctx.fillStyle = clrs[pIdx];
-	ctx.beginPath();
-	ctx.arc(p.x, p.y, r, 0, Math.PI*2);
-	ctx.fill();
-    }
+	{
+	    const pivot = getPivot();
+	    const theta = pivotTheta() - Math.PI/2 + Math.PI*_t;
+	    const p = {
+		x: pivot.x + Math.cos(theta) * Math.sqrt(3)/2,
+		y: pivot.y + Math.sin(theta) * Math.sqrt(3)/2,
+	    };
 
-    {
-	const pivot = getPivot();
-	const theta = pivotTheta() + Math.PI/2 + Math.PI*_t;
-	const p = {
-	    x: pivot.x + Math.cos(theta) * Math.sqrt(3)/2,
-	    y: pivot.y + Math.sin(theta) * Math.sqrt(3)/2,
-	};
+	    ctx.fillStyle = clrs[pIdx];
+	    ctx.beginPath();
+	    ctx.arc(p.x * scale, p.y * scale, r * scale, 0, Math.PI*2);
+	    ctx.fill();
+	}
 
-	ctx.fillStyle = clrs[(pIdx+1)%3];
-	ctx.beginPath();
-	ctx.arc(p.x, p.y, r, 0, Math.PI*2);
-	ctx.fill();
+	{
+	    const pivot = getPivot();
+	    const theta = pivotTheta() + Math.PI/2 + Math.PI*_t;
+	    const p = {
+		x: pivot.x + Math.cos(theta) * Math.sqrt(3)/2,
+		y: pivot.y + Math.sin(theta) * Math.sqrt(3)/2,
+	    };
+
+	    ctx.fillStyle = clrs[(pIdx+1)%3];
+	    ctx.beginPath();
+	    ctx.arc(p.x * scale, p.y * scale, r * scale, 0, Math.PI*2);
+	    ctx.fill();
+	}
     }
 }
 
@@ -93,8 +91,15 @@ function drawFrame() {
     const min = Math.min(w,h);
     ctx.clearRect(-min/2,-min/2,min,min);
 
+    ctx.shadowBlur = 20;
+    ctx.shadowColor = '#0004';
+    ctx.fillStyle = 'hsl(0,0%,75%)';
+    ctx.beginPath();
+    ctx.arc(0, 0, (1/2 + Math.sqrt(3)/2 + r) * 1.2 * scale, 0, Math.PI*2);
+    ctx.fill();
+
     let now = Date.now();
-    t += (now-lastTime)/1500;
+    t += (now-lastTime)/2000;
     lastTime = now;
     if (t >= 1) {
 	t %= 1;
